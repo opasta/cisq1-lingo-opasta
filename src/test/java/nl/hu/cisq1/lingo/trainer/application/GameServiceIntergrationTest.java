1287a054a037f5224d0abaa20e22281a10a7e50f
opasta <opasta@outlook.com>
2021-04-11 18:13:51 +0200

    Last Push

--- src/test/java/nl/hu/cisq1/lingo/trainer/application/GameServiceIntergrationTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/application/GameServiceIntergrationTest.java
@@ -3,12 +3,14 @@ package nl.hu.cisq1.lingo.trainer.application;
 import nl.hu.cisq1.lingo.CiTestConfiguration;
 import nl.hu.cisq1.lingo.trainer.domain.GameStatus;
 import nl.hu.cisq1.lingo.trainer.domain.Progress;
+import org.hibernate.Hibernate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,22 +18,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Import(CiTestConfiguration.class)
+@DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
 class GameServiceIntergrationTest {
-
-    @AfterEach
-    public void clear(){
-        GameService gameService = null;
-    }
 
     @Autowired
     private GameService gameService;
 
-
     @Test
     @DisplayName("starting a new game, so status is playing")
-    void startGame() {
+    void
+    startGame() {
         Progress progress = gameService.startNewGame();
         assertEquals(progress.getStatus(), GameStatus.PLAYING);
+        System.out.println(progress);
     }
-
 }
